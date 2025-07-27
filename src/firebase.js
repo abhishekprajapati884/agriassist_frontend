c412c51a2c1155bbbb6c89764f5d4fdbe3301745
abhishekprajapati884 <20ce01039@iitbbs.ac.in>
2025-07-27 11:32:35 +0530

    removed

--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDjRgkAwy-I-gwnzbFs7DhcT2ljaDc5awM",
-//   authDomain: "windy-bounty-466717-q5.firebaseapp.com",
-//   projectId: "windy-bounty-466717-q5",
-//   storageBucket: "windy-bounty-466717-q5.firebasestorage.app",
-//   messagingSenderId: "454280480783",
-//   appId: "1:454280480783:web:45be75fe732a882967fb7a",
-//   measurementId: "G-8K2MDLX3FW"
-// };
-
-//purvi's cred
 const firebaseConfig = {
-  apiKey: "AIzaSyBLsrlSiDfj1MoWWvipD8mhu80NQ92xjNw",
-  authDomain: "optimum-lodge-395210.firebaseapp.com",
-  projectId: "optimum-lodge-395210",
-  storageBucket: "optimum-lodge-395210.firebasestorage.app",
-  messagingSenderId: "1001728314783",
-  appId: "1:1001728314783:web:c2e393fbc6b3c99afe81aa",
-  measurementId: "G-V1WL2EW3ZQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
